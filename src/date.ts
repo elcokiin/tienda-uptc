48d24e5ab8cdbc744074a14cc340a39d67043b52
Diego Tenjo <tenjodiego961@gmail.com>
2024-10-18 21:59:09 -0500

    First commit (configuration)

---
+++ src/date.ts
@@ -0,0 +1,39 @@
+export function clock(): string {
+    const now: Date = new Date()
+    const options: Intl.DateTimeFormatOptions = {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    }
+    const time: string = new Intl.DateTimeFormat('en-US', options).format(now)
+    
+
+    return time
+}
+
+export function createClock(): HTMLElement {
+    const time = clock()
+    const c = document.createElement('p')
+    c.textContent = time
+    return c
+}
+
+export function date(): string {
+    const now: Date = new Date()
+    const options: Intl.DateTimeFormatOptions = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    const date: string = new Intl.DateTimeFormat('en-US', options).format(now)
+
+    return date
+}
+
+export function createDate(): HTMLElement {
+    const dateElement = document.createElement('p')
+    dateElement.textContent = date()
+
+    return dateElement
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+import { createClock, createDate } from './date'
